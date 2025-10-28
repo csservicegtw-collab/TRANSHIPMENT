9ec21a5683ec94e78f83871ea1750d6c71f3ab76
csservicegtw-collab <customsubgtw@gmail.com>
2025-10-28 21:56:55 +0700

    Update firebase-config.js

--- stuffing/firebase-config.js
+++ stuffing/firebase-config.js
@@ -1,3 +1,4 @@
+
 const firebaseConfig = {
   apiKey: "AIzaSyAu0br1o29T7QM7StyHezHlZ67WiVsTzx0",
   authDomain: "transshipment-8c2da.firebaseapp.com",
@@ -8,5 +9,6 @@ const firebaseConfig = {
   measurementId: "G-21L0CZJ1MC"
 };
 
+// Inisialisasi Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
