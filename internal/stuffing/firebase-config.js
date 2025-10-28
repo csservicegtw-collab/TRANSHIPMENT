95ec2b961ae4f81cec5fcee58f3ddd2904ef230d
csservicegtw-collab <customsubgtw@gmail.com>
2025-10-29 00:25:24 +0700

    Add files via upload

---
+++ internal/stuffing/firebase-config.js
@@ -0,0 +1,17 @@
+// firebase-config.js (compat version - dipanggil dari index.html & vessel.html)
+const firebaseConfig = {
+  apiKey: "AIzaSyAu0br1o29T7QM7StyHezHlZ67WiVsTzx0",
+  authDomain: "transshipment-8c2da.firebaseapp.com",
+  projectId: "transshipment-8c2da",
+  storageBucket: "transshipment-8c2da.firebasestorage.app",
+  messagingSenderId: "997549413633",
+  appId: "1:997549413633:web:b173bddaf4b73cccd13700",
+  measurementId: "G-21L0CZJ1MC"
+};
+
+// Inisialisasi Firebase (compat)
+if (!window.firebase || !firebase.apps || firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
+const db = firebase.firestore();
+console.log("Firebase initialized, Firestore ready.");
